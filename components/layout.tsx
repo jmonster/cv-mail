c5a8dd13d0f5649b00c00caa44c5158c50adcde5
Johnny Domino <jvdomino@gmail.com>
2023-01-05 13:21:59 -0500

    Update layout.tsx

--- components/layout.tsx
+++ components/layout.tsx
@@ -3,7 +3,7 @@ import { ReactNode } from "react"
 const Layout = ({ children }: Record<string, ReactNode>) => {
   return (
     <div className="flex flex-col min-h-screen bg-gray-50 dark:bg-black">
-      <main className="flex-auto w-full max-w-4xl px-2 lg:px-0 mx-auto md:p-8 print:p-0 bg-white text-black dark:bg-gray-900 dark:text-white">
+      <main className="flex-auto w-full max-w-4xl px-2 mx-auto md:p-8 print:p-0 bg-white text-black dark:bg-gray-900 dark:text-white">
         {children}
       </main>
     </div>
